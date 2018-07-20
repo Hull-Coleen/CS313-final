b8104c773357afc256813096c9bb5b13d07b34d5
chhull <ch.hull@verizon.net>
2018-07-20 12:09:25 -0400

    added some more style and input handling

--- index.js
+++ index.js
@@ -46,23 +46,32 @@ app.get('/getExercise', verifyLogin, getExercise);
 app.get('/getWeight', verifyLogin, getWeight);
 app.get('/insert', verifyLogin, insertData);
 
-// 
+/*******************************************************************************
+ * HandleLogout
+ * -----------------------------------------------------------------------------
+ * destroys session variables
+ * Success: 200 {  true }
+ * Failure: 401 {  false }
+ ******************************************************************************/
 function handleLogout(request, response) {
 	var result = {success: false};
-
-	// 
 	if (request.session.userId) {
 		request.session.destroy();
 		result = {success: true};
 	}
-
 	response.json(result);
 }
 
+/*******************************************************************************
+ * GetPulse
+ * -----------------------------------------------------------------------------
+ * Session id variable
+ * Success: 201 { success: 'pulse data'}
+ * Failure: 400 { success: false }
+ *          
+ ******************************************************************************/
 function getPulse(req, res) {
      var id = req.session.userId;
-    console.log(id);
-    console.log(req.session.userId);
     // query database
     db.any('SELECT pulse, day_of_input FROM health WHERE person_id = $1 ORDER BY day_of_input DESC', [id]) // returns promise
       .then((results)=> {
@@ -70,7 +79,6 @@ function getPulse(req, res) {
         res.status(200)
            .json({success:true, pulse: results})
            console.log(results)
-           //.json(results)
       })
       .catch((err)=> {
           console.log(err)
@@ -78,6 +86,15 @@ function getPulse(req, res) {
              .json({success:"Person does not exist."})
       })
 }
+
+/*******************************************************************************
+ * GetExercise
+ * -----------------------------------------------------------------------------
+ * Session id variable
+ * Success: 201 { success: 'exercise data'}
+ * Failure: 400 { success: false }
+ *          
+ ******************************************************************************/
 function getExercise(req, res) {
 	
      var id = req.session.userId;
@@ -94,6 +111,15 @@ function getExercise(req, res) {
              .json({success:"Person does not exist."})
       })
 }
+
+/*******************************************************************************
+ * GetWeight
+ * -----------------------------------------------------------------------------
+ * Session id variable
+ * Success: 201 { success: 'weight data'}
+ * Failure: 400 { success: false }
+ *          
+ ******************************************************************************/
 function getWeight(req, res) {
 
 
@@ -104,7 +130,6 @@ function getWeight(req, res) {
         res.status(200)
            .json({success: true, weight: results})
            console.log(results)
-           //.json(results)
       })
       .catch((err)=> {
           console.log(err)
@@ -113,6 +138,14 @@ function getWeight(req, res) {
       })
 }
 
+/*******************************************************************************
+ * Getuser
+ * -----------------------------------------------------------------------------
+ * Body Params: username, password
+ * Success: 201 { success: 'id'}
+ * Failure: 400 { success: false }
+ *          
+ ******************************************************************************/
 function getUser(req, res) {
 
    var username = req.body.username;
@@ -125,7 +158,6 @@ function getUser(req, res) {
         
         req.session.userId = result.id;
         req.session.name = result.name;
-       // document.getElementById('#greeting').innerText = result.name;
         console.log(req.session.userId);
         res.json({success: true, name: result.name})
     } else {
@@ -140,6 +172,14 @@ function getUser(req, res) {
    })
 }
 
+/*******************************************************************************
+ * InsertData
+ * -----------------------------------------------------------------------------
+ * Params: exercise, time, weight, pulse, date, session ID
+ * Success: 201 { success: 'data inserted'}
+ * Failure: 400 { success: false }
+ *          
+ ******************************************************************************/
 function insertData(req, res) {
 	
 	var url_parts = url.parse(req.url, true);
@@ -167,6 +207,14 @@ function insertData(req, res) {
       })
 }
 
+/*******************************************************************************
+ * CreateUser
+ * -----------------------------------------------------------------------------
+ * Body Params: name, username, password
+ * Success: 201 { success: 'id'}
+ * Failure: 400 { success: false }
+ *          
+ ******************************************************************************/
 function createUser(req, res) {
 
     var name = req.body.name;
@@ -182,15 +230,30 @@ function createUser(req, res) {
         
     }).catch(err => {
         console.log(err);
+        res.status(400)
+             .json({success: false})
     })
     
 }
 
+/*******************************************************************************
+ * LogRequest
+ * -----------------------------------------------------------------------------
+ * Request: url
+ * Displays url requests         
+ ******************************************************************************/
 function logRequest(req, res, next) {
     console.log("Received a request for: " + req.url);
     next();
 
 }
+
+/*******************************************************************************
+ * VerifyLogin
+ * -----------------------------------------------------------------------------
+ * Req: session ID
+ * Checks to ensure id is set moves to the next function        
+ ******************************************************************************/
 function verifyLogin(req, res, next) { 
     console.log("verify" + req.session.userId)
     if(req.session.userId) {
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,8 @@
-//var dateFormat = require('dateformat');
+/*******************************************************************************
+ * Signin
+ * -----------------------------------------------------------------------------
+ * makes the fields needed to signin visible
+ ******************************************************************************/
 function signin() {
 	console.log("inside signin");
 	document.querySelector('#name').style.visibility = "visible";
@@ -6,6 +10,11 @@ function signin() {
 	document.querySelector('input[name="signin"]').style.visibility = "visible";
 }
 
+/*******************************************************************************
+ * CreateAccount
+ * -----------------------------------------------------------------------------
+ * makes the fields needed to create an account visible
+ ******************************************************************************/
 function createAccount() {
 	document.querySelector('#name').style.visibility = "visible";
 	document.querySelector('#pass').style.visibility = "visible";
@@ -13,15 +22,26 @@ function createAccount() {
 	document.querySelector('#username').style.visibility = "visible";
 	document.querySelector('input[name="create"]').style.visibility = "visible";
 }
+
+/*******************************************************************************
+ * GETDATE
+ * -----------------------------------------------------------------------------
+ * get the date from the web and return a fromatted date String
+ ******************************************************************************/
 function getDate () {
 	var d = new Date();
 	var formDate = (d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear();
 	console.log(formDate);
 	return formDate;
-
-	 //return new Date().toDateString();
 }
 
+/*******************************************************************************
+ * Login
+ * -----------------------------------------------------------------------------
+ * Params: name, pass
+ * Success: 200 {  true,  id }
+ * Failure: 401 {  false }
+ ******************************************************************************/
 function login() {
     console.log("login");
 	var username = $("#name").val();
@@ -46,6 +66,14 @@ function login() {
 	});
 }
 
+/*******************************************************************************
+ * GetPulse
+ * -----------------------------------------------------------------------------
+ * Success: 201 { success: 'pulse data'}
+ * Failure: 400 { success: false }
+ *          
+ ******************************************************************************/
+
 function getPulse() {
 	$.get("/getPulse", function(result) {
 		if (result && result.success) {
@@ -62,6 +90,13 @@ function getPulse() {
 	});
 }
 
+/*******************************************************************************
+ * Exercise
+ * -----------------------------------------------------------------------------
+ * Success: 201 { success: 'exercise data'}
+ * Failure: 400 { success: false }
+ *          
+ ******************************************************************************/
 function exercise(e) {
 	console.log("getexercise function");
     $.get("/getExercise", function(result) {
@@ -79,13 +114,20 @@ function exercise(e) {
 	});
 
 }
+
+/*******************************************************************************
+ * Weight
+ * -----------------------------------------------------------------------------
+ * Success: 201 { success: 'weight data'}
+ * Failure: 400 { success: false }
+ *          
+ ******************************************************************************/
 function weight(e) {
-	console.log("getweight function");
+	
     $.get("/getWeight", function(result) {
 		if (result && result.success) {
             $("#status").text("Weight");
             const output = document.querySelector('#title')
-           // console.log("getweight funct" + JSON.stringify(result))
             weightList(result.weight);
             output.innerText = "Weight Results";
 		} else {
@@ -95,11 +137,18 @@ function weight(e) {
 		$("#status").text("Could not get Data. You need to be logged in.");
 	});
 }
+
+/*******************************************************************************
+ * WeightList
+ * -----------------------------------------------------------------------------
+ * Params: array
+ * Creates an unsorted list of the objects in array       
+ ******************************************************************************/
 function weightList(data){
     var objs = data.map(item => {
-		var d = new Date(item.day_of_input);
-		var formDate = (d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear();
-		console.log("date" + formDate);
+	   var d = new Date(item.day_of_input);
+	   var formDate = (d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear();
+	
         return item.weight + " --- " +  formDate;
     })
     console.log(objs);
@@ -111,7 +160,12 @@ function weightList(data){
         div.appendChild(li);
     });
 
-}
+}/*******************************************************************************
+ * ExerciseList
+ * -----------------------------------------------------------------------------
+ * Params: array
+ * Creates an unsorted list of the objects in array       
+ ******************************************************************************/
 function exerciseList(data){
     var objs = data.map(item => {
 		var d = new Date(item.day_of_input);
@@ -128,6 +182,13 @@ function exerciseList(data){
     });
 
 }
+
+/*******************************************************************************
+ * PulseList
+ * -----------------------------------------------------------------------------
+ * Params: array
+ * Creates an unsorted list of the objects in array       
+ ******************************************************************************/
 function pulseList(data){
     var objs = data.map(item => {
 		var d = new Date(item.day_of_input);
@@ -145,13 +206,19 @@ function pulseList(data){
 
 }
 
+/*******************************************************************************
+ * Insert
+ * -----------------------------------------------------------------------------
+ * Params: exercise, time, pulse, weight
+ * Success: 201 { success: 'data inserted'}
+ * Failure: 400 { success: false }     
+ ******************************************************************************/
 function insert(e) {
     console.log("insert function");
     var exercise = $("#exercise").val();
 	var time = $("#time").val();
     var pulse = $("#pulse").val();
 	var weight = $("#weight").val();
-	//var date = $("#date").val();
 	var date = getDate();
 	
 	var params = {
@@ -173,7 +240,13 @@ function insert(e) {
 	});
 }
  
-
+/*******************************************************************************
+ * Logout
+ * -----------------------------------------------------------------------------
+ * Deletes table and session id
+ * Success: 201 { success: 'logged out'}
+ * Failure: 400 { success: false }     
+ ******************************************************************************/
 function logout() {
 	$.post("/logout", function(result) {
 		if (result && result.success) {
@@ -190,6 +263,13 @@ function logout() {
 	});
 }
 
+/*******************************************************************************
+ * Create
+ * -----------------------------------------------------------------------------
+ * Params: username, name, pass
+ * Success: 201 { success: 'logged in'}
+ * Failure: 400 { success: 'Could not create account' }     
+ ******************************************************************************/
 function create(e) {
     
     var username = $("#username").val();
@@ -204,7 +284,7 @@ function create(e) {
     //
 	$.post("/createUser", params, function(result) {
 		if (result && result.success) {
-			console.log("help " + result.name)
+			
 			$("#status").text("Successfully logged in.");
 			document.querySelector('#greeting').innerText = "Welcome " + name;
 			document.querySelector('#name').style.visibility = "hidden";
@@ -215,29 +295,59 @@ function create(e) {
 			document.querySelector('#messagePass1').style.visibility = "hidden";
 	        document.querySelector('input[name="create"]').style.visibility = "hidden";
 		} else {
-			$("#status").text("Error logging in.");
+			$("#status").text("Problem Creating Account");
 		}
+	}).fail(function(result) {
+		$("#status").text("Could not create account. Username must be unique?");
 	});
 
+
 }
+
+/*******************************************************************************
+ * CheckPassword
+ * -----------------------------------------------------------------------------
+ * Check the pass input to make sure the password has 7 character
+ * and at least one number   
+ ******************************************************************************/
+function checkPassword() {
+	var regex = /^(?=\D*\d)\S{7,}$/;
+	if (document.querySelector('input[name="create"]').style.visibility === 'visible') {
+
+	   if( regex.test(document.getElementById('pass').value) ) {
+			 document.getElementById('passWarning').innerHTML = '';
+			 document.querySelector('input[name="create"]').disabled = false;
+	   } else {
+			 document.getElementById('passWarning').innerHTML = 'Password must have at least one number and 7 characters';
+			 document.querySelector('input[name="create"]').disabled = true;
+	   }
+    }
+}
+
+/*******************************************************************************
+ * Check
+ * -----------------------------------------------------------------------------
+ * Checks pass and password to make sure the are the same. Tells 
+ * wether the passwords match or not    
+ ******************************************************************************/
 function check() {      
 
-if (document.querySelector('input[name="create"]').style.visibility === 'visible') {
+   if (document.querySelector('input[name="create"]').style.visibility === 'visible') {
 	//
-	if (document.getElementById('password').value ==
-	  document.getElementById('pass').value) {
-	  document.getElementById('messagePass').style.color = '#000080';
-	  document.getElementById('messagePass').innerHTML = 'matching';
-	   document.getElementById('messagePass1').style.color = '#000080';
-	  document.getElementById('messagePass1').innerHTML = 'matching';
-	} else {
-	  document.getElementById('messagePass').style.color = 'red';
-	  document.getElementById('messagePass').innerHTML = 'not matching';
-	  document.getElementById('messagePass1').style.color = 'red';
-	  document.getElementById('messagePass1').innerHTML = 'not matching';
+	   if (document.getElementById('password').value ==
+	       document.getElementById('pass').value) {
+	          document.getElementById('messagePass').style.color = '#000080';
+	          document.getElementById('messagePass').innerHTML = 'matching';
+	          document.getElementById('messagePass1').style.color = '#000080';
+	          document.getElementById('messagePass1').innerHTML = 'matching';
+	    } else {
+	       document.getElementById('messagePass').style.color = 'red';
+	       document.getElementById('messagePass').innerHTML = 'not matching';
+	       document.getElementById('messagePass1').style.color = 'red';
+	       document.getElementById('messagePass1').innerHTML = 'not matching';
 
+	    }
 	}
-}
 }
 
 
